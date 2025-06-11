acc6ccf64c01c2ef2419399fc77a694cdab5de60
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-06-11 11:32:38 +0530

    some changes done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const loginRoutes = require('./routes/loginRoutes');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const loginRoutes = require("./routes/loginRoutes");
 
 dotenv.config();
 connectDB();
@@ -11,13 +11,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', loginRoutes);
+app.use("/api", loginRoutes);
 
-app.get('/', (req, res) => {
-    res.send("server and db both are running");
+app.get("/", (req, res) => {
+  res.send("server and db both are running");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
+  console.log(`server running on ${PORT}`);
 });
