719b21945a355955f6533cdcebd702d8fcdae494
Sushil321 <sushil.shivsharan009@gmail.com>
2025-06-13 12:02:39 +0530

    get

--- routes/RecruiterRoutes.js
+++ routes/RecruiterRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const createRecruiter = require('../controllers/createRecruiterController');
+const {
+  createRecruiter,
+  getSingleRecruiter,
+  updateRecruiter,
+  deleteRecruiter,
+  getAllRecruiters
+} = require('../controllers/createRecruiterController');
 const authenticate = require('../middlewares/authenticate');
 
 
 router.post('/createrecruiter', authenticate('super_admin'), createRecruiter);
+router.get('/getrecruiter/:id', authenticate('super_admin'), getSingleRecruiter);
+router.put('/updaterecruiter/:id', authenticate('super_admin'), updateRecruiter);
+router.delete('/deleterecruiter/:id', authenticate('super_admin'), deleteRecruiter);
+router.get('/getallrecruiters', authenticate('super_admin'), getAllRecruiters);
 
 module.exports = router;
