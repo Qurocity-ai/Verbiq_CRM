be0aa387c8f8211810833404b961068bbc74f999
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-06-14 15:01:51 +0530

    Get Client information is done

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,21 +1,32 @@
+const clientmodel = require("../models/clientmodel");
 
-const clientmodel=require('../models/clientmodel')
-
-
-const createClient=async (req,res) => {
-
-    try {
-         const client=new clientmodel(req.body);
+const createClient = async (req, res) => {
+  try {
+    const client = new clientmodel(req.body);
     await client.save();
-    res.status(200).json({success:true,message:"client requirement created successfully",client})
-    
-        
-    } 
-    catch (error) {
-        res.status(500).json({success:false,message:"Error while creating client requirement",error:error.message})
-    }
-   
+    res.status(200).json({
+      success: true,
+      message: "client requirement created successfully",
+      client,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while creating client requirement",
+      error: error.message,
+    });
+  }
+};
 
-}
+const getClient = async (req, res) => {
+  try {
+    const clients = await clientmodel.find({});
 
-module.exports={createClient};
+    res.status(200).json({ success: true, count: clients.length, clients });
+  } catch (err) {
+    console.log("Error fetching Client", err);
+    res.status(500).json({ success: false, err: "Internal server error" });
+  }
+};
+
+module.exports = { createClient, getClient };
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,10 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { createClient, getClient } = require("../controllers/clientController");
+const authenticate = require("../middlewares/authenticate");
 
-const express=require('express');
-const router=express.Router();
-const {createClient}=require('../controllers/clientController');
-const authenticate=require('../middlewares/authenticate')
+router.post("/createClient", authenticate("super_admin"), createClient);
+router.post("/getClient", authenticate("super_admin"), getClient);
 
-
-router.post('/createClient',authenticate("super_admin"),createClient);
-
-module.exports=router; //
+module.exports = router; //
