2fcfb59e6fac43163f7bd56e8e80ab424db90f84
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-06-27 21:31:38 +0530

    some changes done

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -9,21 +9,21 @@ const {
 } = require("../controllers/candidateController.js");
 const authenticate = require("../middlewares/authenticate");
 
-router.post("/createCandidate", authenticate("super_admin"), createCandidate);
+router.post("/createCandidate", createCandidate);
 router.get(
   "/getCandidate",
 
   getCandidate
 );
-router.get("/getCandidate/:id", authenticate("super_admin"), getCandidateById);
+router.get("/getCandidate/:id", getCandidateById);
 router.delete(
   "/deleteCandidate/:id",
-  authenticate("super_admin"),
+
   deleteCandidateById
 );
 router.put(
   "/updateCandidate/:id",
-  authenticate("super_admin"),
+
   updateCandidateById
 );
 
