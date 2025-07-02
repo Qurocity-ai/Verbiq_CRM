82c497e5914991e01107e5c7fe1c6a3c8bdf4d95
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-02 17:03:39 +0530

    some changes done

--- routes/RecruiterRoutes.js
+++ routes/RecruiterRoutes.js
@@ -1,26 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const {
- createRecruiter,
+  createRecruiter,
   getSingleRecruiter,
   updateRecruiter,
   deleteRecruiter,
   getAllRecruiters,
-  getTopRecruiters,        
+  getTopRecruiters,
   searchRecruiters,
-  getAllRecruiterList
-} = require('../controllers/createRecruiterController');
-const authenticate = require('../middlewares/authenticate');
+  getAllRecruiterList,
+} = require("../controllers/createRecruiterController");
+const authenticate = require("../middlewares/authenticate");
 
 router.post("/createrecruiter", authenticate("super_admin"), createRecruiter);
 
-router.post('/createrecruiter', authenticate('super_admin'), createRecruiter);
-router.get('/getrecruiter/:id', authenticate('super_admin'), getSingleRecruiter);
-router.put('/updaterecruiter/:id', authenticate('super_admin'), updateRecruiter);
-router.delete('/deleterecruiter/:id', authenticate('super_admin'), deleteRecruiter);
-router.get('/getallrecruiters', authenticate('super_admin'), getAllRecruiters);
-router.get('/toprecruiters',authenticate('super_admin'), getTopRecruiters);
-router.get('/searchrecruiters',authenticate('super_admin'), searchRecruiters);
-router.get('/allrecruiters',authenticate('super_admin'), getAllRecruiterList);
+router.post("/createrecruiter", authenticate("super_admin"), createRecruiter);
+router.get(
+  "/getrecruiter/:id",
+  authenticate("super_admin"),
+  getSingleRecruiter
+);
+router.put(
+  "/updaterecruiter/:id",
+  authenticate("super_admin"),
+  updateRecruiter
+);
+router.delete(
+  "/deleterecruiter/:id",
+  authenticate("super_admin"),
+  deleteRecruiter
+);
+router.get("/getallrecruiters", authenticate("super_admin"), getAllRecruiters);
+router.get("/toprecruiters", getTopRecruiters);
+router.get("/searchrecruiters", authenticate("super_admin"), searchRecruiters);
+router.get("/allrecruiters", authenticate("super_admin"), getAllRecruiterList);
 
 module.exports = router;
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -5,22 +5,18 @@ const {
   getClient,
   getClientById,
   deleteClientById,
-  updateClientById
+  updateClientById,
 } = require("../controllers/clientController");
 const authenticate = require("../middlewares/authenticate");
 
 router.post("/createClient", authenticate("super_admin"), createClient);
-router.get("/getClient", authenticate("super_admin"), getClient);
+router.get("/getClient", getClient);
 router.get("/getClient/:id", authenticate("super_admin"), getClientById);
 router.delete(
   "/deletedClient/:id",
   authenticate("super_admin"),
   deleteClientById
 );
-router.put(
-  "/updateClient/:id",
-  authenticate("super_admin"),
-  updateClientById
-);
+router.put("/updateClient/:id", authenticate("super_admin"), updateClientById);
 
 module.exports = router; //
