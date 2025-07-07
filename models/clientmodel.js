883d7b327d6c01f359d58ddbf687247d2ac0baaa
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-07 20:44:39 +0530

    some changes in client model

--- models/clientmodel.js
+++ models/clientmodel.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const clientSchema = new mongoose.Schema({
+const clientSchema = new mongoose.Schema(
+  {
     clientName: { type: String, required: true },
     clientEmail: { type: String, required: true },
-    contactName: { type: String, required: true },
-    contactEmail: { type: String, required: true },
-    contactNumber: { type: String, required: true },
-    jobTitle: { type: String, required: true },
-    location: { type: String, required: true },
-    employmentType: { type: [String], required: true },
-    workMode: { type: [String], required: true },
-    minCTC: { type: Number, required: true },
-    maxCTC: { type: Number, required: true },
-    noticePeriod: { type: String, required: true },
-    language: { type: String, required: true },
-    proficiency: { type: String, required: true }
-}, { timestamps: true });
 
-const Client = mongoose.model('Client', clientSchema);
+    contactNumber: { type: String, required: true },
+    Role: { type: String, required: true },
+    // location: { type: String, required: true },
+    // employmentType: { type: [String], required: true },
+    // workMode: { type: [String], required: true },
+    // minCTC: { type: Number, required: true },
+    // maxCTC: { type: Number, required: true },
+    // noticePeriod: { type: String, required: true },
+    // language: { type: String, required: true },
+    // proficiency: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+const Client = mongoose.model("Client", clientSchema);
 module.exports = Client;
