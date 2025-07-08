07d2f1720de154529b7eca3f7c6c3ffb24059704
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-08 10:37:46 +0530

    cors are set

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Or '*' to allow all origins
+  })
+);
 app.use(express.json());
 app.use("/api", loginRoutes);
 app.use("/api", RecruiterRoutes);
